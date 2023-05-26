0334f5f6d61a2a49113467a36d631bea312f3225
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-05-26 17:01:57 +0400

    7.09

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+});
+
+
+// let counter = 1;
+// setInterval(function(){
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
     
-    if(counter > 6){
-        counter = 1;
-    }
-}, 10000);
+//     if(counter > 6){
+//         counter = 1;
+//     }
+// }, 10000);

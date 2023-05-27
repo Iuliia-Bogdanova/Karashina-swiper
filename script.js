903b011a3992e8af030851d4db10af5b9fc7d031
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-05-27 23:17:48 +0400

    bullets done 9.49

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ new Swiper('.image-slider', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        // номер слайда на буллете
+        // renderBullet: function (index, className) {
+        //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+        // },
+    },
 });
 
 
